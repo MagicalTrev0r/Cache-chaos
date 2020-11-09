eccdcf09aa1d7acb924f8b69b58bd26bbfd50936
MagicalTrev0r <72901849+MagicalTrev0r@users.noreply.github.com>
2020-11-09 03:27:51 +0000

    Update SimpleWallet.cpp

--- src/SimpleWallet/SimpleWallet.cpp
+++ src/SimpleWallet/SimpleWallet.cpp
@@ -1373,7 +1373,7 @@ bool simple_wallet::optimize_outputs(const std::vector<std::string>& args) {
     std::vector<CryptoNote::WalletLegacyTransfer> transfers;
     std::vector<CryptoNote::TransactionMessage> messages;
     std::string extraString;
-    uint64_t fee = CryptoNote::parameters::MINIMUM_FEE;
+    uint64_t fee = CryptoNote::parameters::MINIMUM_FEE_V1;
     uint16_t mixIn = 0;
     uint64_t unlockTimestamp = 0;
     uint64_t ttl = 0;
@@ -1441,7 +1441,7 @@ bool simple_wallet::optimize_all_outputs(const std::vector<std::string>& args) {
       std::vector<CryptoNote::WalletLegacyTransfer> transfers;
       std::vector<CryptoNote::TransactionMessage> messages;
       std::string extraString;
-      uint64_t fee = CryptoNote::parameters::MINIMUM_FEE_V1;
+      uint64_t fee = CryptoNote::parameters::MINIMUM_FEE_V2;
       uint16_t mixIn = 0;
       uint64_t unlockTimestamp = 0;
       uint64_t ttl = 0;
